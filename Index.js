fcf46144272503db56d691a131ca4c2dcc109d78
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-21 18:15:47 +0500

    JS class 1

---
+++ Index.js
@@ -0,0 +1,24 @@
+console.log('Hello World!');
+alert("GIve Acess to Photos");
+console.warn("This is a warning");
+console.error("This is an error");
+
+// variables in JavaSript
+var number1 = 34;
+var number2 = 56;
+console.log(number1 + number2);
+
+var fnum1= 30.5;
+var fnum2 = 45.5;
+
+var str1 = "This is a string 1";
+var str2= 'this is a string 2';
+
+// Objects in JavaScript
+ var marks={
+    hammad: 34,
+    ali: 45,
+    hamza: 67
+ }
+    console.log(marks);
+    
