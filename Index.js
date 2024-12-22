62827206aa3416e71a334facd9981e81f3fd03a6
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-22 13:32:27 +0500

    variables in Javasript

--- Index.js
+++ Index.js
@@ -6,7 +6,7 @@ console.error("This is an error");
 // variables in JavaSript
 var number1 = 34;
 var number2 = 56;
-console.log(number1 + number2);
+// console.log(number1 + number2);
 
 var fnum1= 30.5;
 var fnum2 = 45.5;
@@ -20,5 +20,13 @@ var str2= 'this is a string 2';
     ali: 45,
     hamza: 67
  }
-    console.log(marks);
-    
+   //  console.log(marks);
+
+    var bool1 = true;
+    var bool2 = false;
+
+   //  Array in Javasript
+    var arr=[2,"aitzaz", 4,6,3,66,2];
+   //  console.log(arr);
+   console.log(arr[1]);
+   
