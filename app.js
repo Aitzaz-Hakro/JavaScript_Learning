95836c23c5d725dd92e09b1b3505d995cd6bd2a3
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-27 16:18:27 +0500

    Add boolean value checks for various data types in app2.js

--- app.js
+++ app.js
@@ -1,64 +1,68 @@
-console.log('Hello World!');
-// alert("GIve Acess to Photos");
+// console.log('Hello World!');
+// // alert("GIve Acess to Photos");
 
-console.warn("This is a warning");
-console.error("This is an error");
+// console.warn("This is a warning");
+// console.error("This is an error");
 
-// variables in JavaSript
-var number1 = 34;
-var number2 = 56
-// console.log(number1 + number2);
+// // variables in JavaSript
+// var number1 = 34;
+// var number2 = 56
+// // console.log(number1 + number2);
 
-var fnum1= 30.5;
-var fnum2 = 45.5;
+// var fnum1= 30.5;
+// var fnum2 = 45.5;
 
-var str1 = "This is a string 1";
-var str2= 'this is a string 2';
+// var str1 = "This is a string 1";
+// var str2= 'this is a string 2';
 
-// Objects in JavaScript
- var marks={
-    hammad: 34,
-    ali: 45,
-    hamza: 67
- }
-   //  console.log(marks);
+// // Objects in JavaScript
+//  var marks={
+//     hammad: 34,
+//     ali: 45,
+//     hamza: 67
+//  }
+//    //  console.log(marks);
 
-    var bool1 = true;
-    var bool2 = false;
+//     var bool1 = true;
+//     var bool2 = false;
 
-   //  Array in Javasript
-    var arr=[2,"aitzaz", 4,6,3,66,2];
-   //  console.log(arr);
-   // console.log(arr[1]);
+//    //  Array in Javasript
+//     var arr=[2,"aitzaz", 4,6,3,66,2];
+//    //  console.log(arr);
+//    // console.log(arr[1]);
 
-   var und=undefined;
-   var n="aitzaz";
+//    var und=undefined;
+//    var n="aitzaz";
    
    
-   // ";" is optional in JavaSript   
-   console.log(n)
-   console.log(typeof n)
+//    // ";" is optional in JavaSript   
+//    console.log(n)
+//    console.log(typeof n)
 
-   var num1="4"-"2"+"4";
-   // output is 24
-   console.log(num1);
+//    var num1="4"-"2"+"4";
+//    // output is 24
+//    console.log(num1);
     
-   document.write("<h1>"+ arr[1]+ "</h1>")
+//    document.write("<h1>"+ arr[1]+ "</h1>")
 
    // prompt is used to take input from user
    // var Name=prompt("Enter your name");
-   // console.log(Name);
-   var gender= prompt("Enter your Gender: "); 
-   if(gender=="Male" ||gender=="male"){
-      console.log("Welcome Sir");
-      document.write("<h1>Hello Sir </h1>");
-   }
-   else if(gender=="female"||gender=="Female"){
-      console.log("Welcome Mam");
-   }
-   Swal.fire({
-      title: "Drag me!",
-      icon: "success",
-      draggable: true
-    });
+   // // console.log(Name);
+   // var gender= prompt("Enter your Gender: "); 
+   // if(gender=="Male" ||gender=="male"){
+   //    console.log("Welcome Sir");
+   //    document.write("<h1>Hello Sir </h1>");
+   // }
+   // else if(gender=="female"||gender=="Female"){
+   //    console.log("Welcome Mam");
+   // }
+   // Swal.fire({
+   //    title: "Drag me!",
+   //    icon: "success",
+   //    draggable: true
+   //  });
 
+
+    var  name=prompt("Enter your name: ") || "Not Entered";
+    console.log(name);
+    
--- app2.js
+++ app2.js
@@ -2,5 +2,19 @@
 var num1=3;
 var num2="3";
 
-console.log(num1==num2);//this checks the value only
-console.log(num1===num2); //this checks the value and datatype both
+// console.log(num1==num2);//this checks the value only
+// console.log(num1===num2); //this checks the value and datatype both
+
+var string1="This is a string";
+
+Boolean(string1); //true  (other mehtod is !!string1)
+
+// All strings have true value except 6.
+
+Boolean(""); //false
+Boolean(0); //false
+Boolean(undefined); //false
+Boolean(null); //false
+Boolean(NaN); //false
+Boolean(false); //false
+
