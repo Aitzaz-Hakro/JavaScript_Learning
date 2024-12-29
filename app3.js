9a7f75d016354a6b4cfa565589ba346ce5eb78b5
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-29 19:55:58 +0500

    Enhance app3.js with splice and slice method examples; update index.html with lesson header

--- app3.js
+++ app3.js
@@ -12,12 +12,29 @@
 // console.log(copyarray)
 
 
+
+
+// ******************  SPLICE AND SLICE METHODS  ******************
+
 var Cities=["Hyderabad","Karachi","Multan","Lahore","Islamabad"];
 console.log("Cities before==>",Cities);
 
-var CitiesAdd=Cities.splice(5,3,"Faisalabad","Quetta") ;
+var CitiesAdd=Cities.splice(8,2,"Faisalabad","Quetta") ;
+
 // it will add cities at index 5 because index 5 is not present in array 
 // if the index is available then splice will replace the value of that index with the given value
 
-console.log("Cities After==>",Cities);
 
+// var CitiesCopy=Cities.slice(1,2,"Faisalabad","Quetta") ;
+
+console.log("Cities Added==>",CitiesAdd);
+
+// console.log("Cities Copy==>",CitiesCopy); 
+
+
+console.log("Cities After==>",Cities);  //After Addition
+
+
+
+// Slice is used to copy the array
+// Splice is used to add or remove the elements from the array
