68fb0391b9ad641a8f061d4171e848f5e0b6aa0a
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-01-27 23:03:33 +0500

    ]Update array swapping logic to avoid using a third variable and clean up commented code

--- class29/elementswap.js
+++ class29/elementswap.js
@@ -1,11 +1,23 @@
 
-var array=["aitzaz ","zain", "Civic"];
+// var array=["aitzaz ","zain", "Civic"];
 
-console.log(array);
-console.log("array before==>",array);
+// console.log(array);
+// console.log("array before==>",array);
 
-var temp=array[0];
-array[0]=array[2];
-array[2]=temp;
+// var temp=array[0];
+// array[0]=array[2];
+// array[2]=temp;
 
-console.log("array after==>",array);
+// console.log("array after==>",array);
+
+// Task is to swap without using 3rd variable
+ 
+ var array=["aitzaz ","zain", "Civic"]; 
+    console.log("array before==>",array);
+
+    array[0]=array[0]+array[2];
+    array[2]=array[0].substring(0,array[0].length-array[2].length);
+    array[0]=array[0].substring(array[2].length);
+    console.log("array after==>",array);
+    
+    
