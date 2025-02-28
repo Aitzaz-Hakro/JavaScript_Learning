5a1d3d6cde8d5c9bd11762443fe96bbc5b0b3d34
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-02-28 20:42:24 +0500

    javascript class 30 learnings

---
+++ class30/app.js
@@ -0,0 +1,10 @@
+
+// TO print random number between 0-1
+var random = Math.random();
+var random2 = Math.random()*10;
+
+console.log("Random Number between 0-1")
+console.log(random);
+console.log("Random Number between 0-10")
+console.log(Math.ceil(random2));
+
