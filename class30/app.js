9059154f7f3015faf851598202b5048d3d757c72
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-03-01 18:32:52 +0500

    javascript functions

--- class30/app.js
+++ class30/app.js
@@ -1,10 +1,34 @@
 
 // TO print random number between 0-1
-var random = Math.random();
-var random2 = Math.random()*10;
+// var random = Math.random();
+// var random2 = Math.random()*10;
 
-console.log("Random Number between 0-1")
-console.log(random);
-console.log("Random Number between 0-10")
-console.log(Math.ceil(random2));
+// console.log("Random Number between 0-1")
+// console.log(random);
+// console.log("Random Number between 0-10")
+// console.log(Math.ceil(random2));
 
+/*********************************************************/
+
+
+// Converting string to integers and decimals
+
+// +"343242"
+
+// parseInt("20440");
+// parseInt("20440.324");
+
+
+// parseFloat("20440.343242");
+
+var Num=2324;
+
+// converts number to string
+// console.log(Num.toString());
+
+/*********************************************************/
+
+// controlling the length of decimal values
+var decii=23.334;
+
+console.log(decii.toFixed(2));
