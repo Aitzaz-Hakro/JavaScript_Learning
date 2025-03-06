1959f5b9a5bd74cdbcf712c6c9e227d2864fa89b
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-03-06 18:19:09 +0500

    Add random password generator and update HTML styling for Class 32

--- class31-32/app.js
+++ class31-32/app.js
@@ -1,4 +1,16 @@
-var name = "Aitzaz Hassan";
+// var name = "Aitzaz Hassan";
 
-console.log(name.split(" "));
-console.log(["AITZAZ", "HASSAN"].join(" "));
+// console.log(name.split(" "));
+// console.log(["AITZAZ", "HASSAN"].join(" "));
+
+
+// Class 32 task random password generator
+
+var randomnum= "aHAFsfs43GL@#$(#)sfdfWEQP16289";
+
+var randompass="";
+for(var i=0;i<10;i++){
+   randompass+=randomnum[Math.floor(Math.random()*randomnum.length)];
+
+}
+console.log(randompass);
