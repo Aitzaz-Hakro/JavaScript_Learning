7864381396fddde72e9fabe79c2eb4ca1c0ee9bc
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-03-16 19:50:44 +0500

    Add form submission functionality. using GetElementbyID

--- class34-Functions/app.js
+++ class34-Functions/app.js
@@ -7,9 +7,23 @@
 // hell0();
 
 
-function nameShow(name) {
-    console.log('Hello ' + name +"!");
-    // document.write('Hello ' + name +"!");
+// function nameShow(name) {
+//     console.log('Hello ' + name +"!");
+//     // document.write('Hello ' + name +"!");
+// }
+// var entername = prompt('Enter your name');
+// nameShow(entername);
+
+
+
+function submitForm(){
+
+  var name = document.getElementById('name');
+  var email= document.getElementById('email');
+
+  console.log('Name: ' + name.value + '\n' + 'Email: ' + email.value);
+//   document.getElementById('result').innerHTML = 'Name: ' + name.value + '<br>' + 'Email: ' + email.value;
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+  
 }
-var entername = prompt('Enter your name');
-nameShow(entername);
