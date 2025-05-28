9eecc688be333c17b03ae95334615ebedafb28ce
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-05-28 16:12:35 +0500

    learnt this keyword and FORIN Loop in Objects

--- objects/app.js
+++ objects/app.js
@@ -10,16 +10,49 @@
 // console.log(obb.hello); // Output: hello Aitzaz Hassan
 
 
- function makeUser(name, age) {
-  return {
-    name, // same as name: name
-    age,  // same as age: age
-    // ...
-  };
+//  function makeUser(name, age) {
+//   return {
+//     name, // same as name: name
+//     age,  // same as age: age
+//     // ...
+//   };
+// }
+
+// let user=makeUser("Aitzaz",23);
+// alert(user.age + " "+user.name); 
+
+// For in Loop in Objects 
+
+// let ClientsObj={
+//     name: "Aitzaz Hassan",
+//     age : 20,
+//     workedfor: "webdev"
+// }
+
+// for(let key in ClientsObj){
+//   console.log(`${key}: ${ClientsObj[key]}`);
+//  }
+
+
+
+
+
+             // This keyword in Objects
+let user = { name: "John" };
+let admin = { name: "Admin" };
+
+function sayHi() {
+  alert( this.name );
 }
 
-let user=makeUser("Aitzaz",23);
-alert(user.age + " "+user.name); 
+// use the same function in two objects
+user.f = sayHi;
+admin.f = sayHi;
 
+// these calls have different this
+// "this" inside the function is the object "before the dot"
+user.f(); // John  (this == user)
+admin.f(); // Admin  (this == admin)
 
+admin['f'](); // Admin (dot or square brackets access the method – doesn't matter)
 
