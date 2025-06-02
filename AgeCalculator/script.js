85f71f60b04fd9aa28ad9894838aa893795edf9f
Aitzaz Hassan <aitzazhakro123@gmail.com>
2025-06-03 00:53:29 +0500

    Age Calculator Project Completed

--- AgeCalculator/script.js
+++ AgeCalculator/script.js
@@ -2,18 +2,24 @@ var birth= document.getElementById("birthdate");
 var age= document.querySelector(".ageResult");
 var btn= document.querySelector(".btn");
 
-var currDate= new Date();
+btn.addEventListener("click", CalculateAge);
+var currDate= new Date().toDateString();
+console.log(currDate);
 
 
 function CalculateAge(){
-  var birthdate= new Date(birth.ariaValueNow);
+  age.innerHTML=""; // 
+  var dob= new Date(birth.value).toDateString();
+  if(birth.value===""){
+    age.innerHTML="Please enter your birthdate";
+  }
+  else if(new Date(birth.value)>= new Date()){
+    age.innerHTML=`Incorrect Date of Birth`; 
+  } 
+  else{
+      var result= new Date().getFullYear() - new Date(dob).getFullYear();
+      age.innerHTML=`Your age is ${result} years`;
+  }
+  }  
 
-
-  console.log(currDate.getFullYear());
-  // console.log(age);
-  
-  console.log(birth.ariaValueNow);
-  // console.log(birthdate);
-  
-}
   
